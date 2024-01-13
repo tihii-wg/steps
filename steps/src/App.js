dfacc1fbc51ca631eaae2cc5271395b1b8cc57ad
Vova <podolian@gmail.com>
2024-01-13 18:08:24 +0200

    new

--- steps/src/App.js
+++ steps/src/App.js
@@ -5,28 +5,38 @@ const message = ["Learn HTML and CSS", "Learn JS", "Learn React"];
 
 function App() {
   const [steps, setSteps] = useState(1);
+  const [isClose, setIsClose] = useState(true);
 
   const previousHandler = () => {
     if (steps > 1) setSteps(steps - 1);
   };
 
-  const NextHandler = () => {
+  const nextHandler = () => {
     if (steps < 3) setSteps(steps + 1);
   };
 
+	const isCloseHandler = () => {
+		setIsClose(!isClose)
+	}
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={steps >= 1 ? "active" : ""}>1</div>
-        <div className={steps >= 2 ? "active" : ""}>2</div>
-        <div className={steps >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">{message[steps - 1]}</p>
-      <div className="buttons">
-        <button onClick={previousHandler}>Previous</button>
-        <button onClick={NextHandler}>Next</button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={isCloseHandler}>&times;</button>
+      {isClose && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={steps >= 1 ? "active" : ""}>1</div>
+            <div className={steps >= 2 ? "active" : ""}>2</div>
+            <div className={steps >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">{message[steps - 1]}</p>
+          <div className="buttons">
+            <button onClick={previousHandler}>Previous</button>
+            <button onClick={nextHandler}>Next</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
